1b52adc8c57019fbde96f33a5dbc663c0eb875fc
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-21 15:03:54 +0000

    Create the styling for the header and hero sections

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import HeaderStyles from './styles.module.scss';
 function Header() {
   return (
-    <div>
+    <div className={HeaderStyles.header}>
       <div className={HeaderStyles.logo}>
         <h1>Startup</h1>
       </div>
       <div className={HeaderStyles.navLinks}>
         <ul>
-          <li>Home</li>
+          <li className={HeaderStyles.activeLink}>Home</li>
           <li>About</li>
           <li>Features</li>
           <li>Overview</li>
@@ -17,7 +17,7 @@ function Header() {
           <li>Contact</li>
         </ul>
       </div>
-      <div className={HeaderStyles.loginRegisterDiv}>
+      <div className={HeaderStyles.loginRegister}>
         <ul>
           <li className={HeaderStyles.loginLink}>Login</li>
           <li className={HeaderStyles.registerLink}>Register</li>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import HeroStyles from './styles.module.scss';
+import HeroImage from '../../images/hero.png';
+import BottomHeroImg from '../../images/bottomHero.png';
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div className={HeroStyles.container}>
+        <div className={HeroStyles.imageTextContainer}>
+          <div className={HeroStyles.textContainer}>
+            <h2>It's Time To Create Your</h2>
+            <h1>Own Business Scheme</h1>
+            <div className={HeroStyles.description}>
+              <p>
+                Celebrated delightful and especially increasing instrument am.
+              </p>
+              <p>Indulgence contrasted sufficent.</p>
+            </div>
+            <button>Get Started</button>
+          </div>
+          <div className={HeroStyles.imageContainer}>
+            <img src={HeroImage} alt="hero" />
+          </div>
+        </div>
+      </div>
+      <div className={HeroStyles.bottomHero}>
+        <img src={BottomHeroImg} alt="bottom hero" />
+      </div>
+    </>
+  );
 }
 
 export default Hero;
